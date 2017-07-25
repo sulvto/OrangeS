987cb4f3280dc6a96a6532d9f88ec5c69b91c83a
sulvto <lpetrel@outlook.com>
2017-07-25 23:13:43 +0800

    update c5

--- c5/loader.asm
+++ c5/loader.asm
@@ -344,40 +344,7 @@ PM_START:
 
 %include    "lib.inc"
 ;---------------------------------------------------------------------
-; 内存拷贝
-MemCpy:
-        push ebp
-        mov ebp,esp
-        
-        push esi
-        push edi
-        push ecx
 
-        mov edi,[ebp + 8]
-        mov esi,[ebp + 12]
-        mov ecx,[ebp + 16]
-    .1:
-        cmp ecx,0
-        jz .z
-        
-        mov al,[ds:esi]
-        inc esi
-        
-        mov byte [es:edi],al
-        inc edi
-        
-        dec ecx
-        jmp .1
-    .2:
-        mov eax,[ebp + 8]
-        pop ecx
-        pop edi
-        pop esi
-        mov esp,ebp
-        pop ebp
-
-        ret
-;---------------------------------------------------------------------
 ; 显示内存信息
 DispMemInfo:
         push esi
