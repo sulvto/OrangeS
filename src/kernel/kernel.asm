b0f5939a248854b731d9f70b43d7458c063fdd47
sulvto <lpetrel@outlook.com>
2017-07-29 22:23:07 +0800

    c5

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -12,6 +12,7 @@ extern exception_handler
 extern gdt_ptr
 extern idt_ptr
 extern disp_pos
+extern spurious_irq
 
 [section .bss]
 StackSpace      resb    2 * 1024
@@ -39,6 +40,24 @@ global general_protection
 global page_fault
 global copr_error
 
+global hwint00
+global hwint01
+global hwint02
+global hwint03
+global hwint04
+global hwint05
+global hwint06
+global hwint07
+global hwint08
+global hwint09
+global hwint10
+global hwint11
+global hwint12
+global hwint13
+global hwint14
+global hwint15
+
+
 _start:     
         mov esp,StackTop
 
@@ -53,13 +72,91 @@ _start:
         jmp SELECTOR_KERNEL_CS:csinit
 
 csinit:
-;        ud2
-        jmp 0x40:0
-        ; push 0
-        ; popfd
-
+        sti
         hlt
 
+; 中断和异常 --硬件中断
+; --------------------------
+%macro hwint_master 1
+        push %1
+        call spurious_irq
+        add esp,4
+        hlt
+%endmacro
+; --------------------------
+ALIGN 16
+hwint00:                ; Interrupt routine for irq 0 (the clock).
+        hwint_master    0
+
+ALIGN 16
+hwint01:                ; Interrupt routine for irq 1 (keyboard).
+        hwint_master    1
+
+ALIGN 16
+hwint02:                ; Interrupt routine for irq 2 (cascade!).
+        hwint_master    2
+
+ALIGN 16
+hwint03:                ; Interrupt routine for irq 3 (second serial).
+        hwint_master    3
+
+ALIGN 16
+hwint04:                ; Interrupt routine for irq 4 (first serial).
+        hwint_master    4
+
+ALIGN 16
+hwint05:                ; Interrupt routine for irq 5 (XT winchester).
+        hwint_master    5
+
+ALIGN 16
+hwint06:                ; Interrupt routine for irq 6 (floppy).
+        hwint_master    6
+
+ALIGN 16
+hwint07:                ; Interrupt routine for irq 7 (printer).
+        hwint_master    7
+
+; --------------------------
+%macro hwint_slave  1
+        push %1
+        call spurious_irq
+        add esp,4
+        hlt
+%endmacro
+; --------------------------
+ALIGN 16
+hwint08:                ; Interrupt routine for irq 8 (realtime clock). 
+        hwint_slave     8
+
+ALIGN 16
+hwint09:                ; Interrupt routine for irq 9 (irq 2 redirected). 
+        hwint_slave     9
+
+ALIGN 16
+hwint10:                ; Interrupt routine for irq 10 . 
+        hwint_slave     10
+
+ALIGN 16
+hwint11:                ; Interrupt routine for irq 11 . 
+        hwint_slave     11
+
+ALIGN 16
+hwint12:                ; Interrupt routine for irq 12 . 
+        hwint_slave     12
+
+ALIGN 16
+hwint13:                ; Interrupt routine for irq 13 (FPU exception). 
+        hwint_slave     13
+
+ALIGN 16
+hwint14:                ; Interrupt routine for irq 14 (AT winchester). 
+        hwint_slave     14
+
+ALIGN 16
+hwint15:                ; Interrupt routine for irq 15. 
+        hwint_slave     15
+
+; 中断和异常 -- 异常
 divide_error:
         push    0xFFFFFFFF  ; no err code
         push    0           ; vector_no = 0
